735e72c9579fc3daf5cb55c5372ca03614ea64f5
Amruta1382 <ammupatil456@gmail.com>
2024-01-13 15:56:10 +0530

    datatype and variable

---
+++ questions.java
@@ -0,0 +1,14 @@
+1. syntax of initializating the variable?
+ans : type variableName=value;
+
+2. how to declare a variable?
+ans : type variableName=value;
+
+3. what is variable?
+ans : variable is the data containers that save the data values during program execution.
+
+4. what is data type?
+ans : It represents the size and different values that can be stored in a variable.
+
+5. what are primative datatypes?
+ans : int, byte, short, long, float, double, boolean and char.
