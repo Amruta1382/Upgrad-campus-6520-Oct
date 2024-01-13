e86f8ae8a0db845cb37212b94178cac06dd6f69c
Amruta1382 <ammupatil456@gmail.com>
2024-01-13 14:24:44 +0530

    first commit

---
+++ Assignment1.java
@@ -0,0 +1,14 @@
+1. what is the git repository?
+ans: A repository is a file structure where git stores all the project based files.
+
+2. what is the command to add files?
+ans: git add <file_name>
+
+3. what is thecommand used to delete a branch?
+ans: git branch -d [head]
+
+4. what does the git command do?
+tracking changes in any set of files.
+
+5. what is the command to add a message to a commit?
+ans: git commit -m "write a commit".
